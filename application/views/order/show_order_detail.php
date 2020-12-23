b5f8da5438e749f83cb7b6725b73b97d89cf7fe5
jay <somkriet5769@gmail.com>
2020-12-23 16:08:34 +0700

    Update show_order_detail.php

--- application/views/order/show_order_detail.php
+++ application/views/order/show_order_detail.php
@@ -67,6 +67,392 @@
     -webkit-animation-timing-function: ease-out;
     animation-timing-function: ease-out;
 }
+
+
+.m-b-md {
+    margin-bottom: 20px;
+}
+.m-t-md {
+    margin-top: 20px;
+}
+
+
+/*.fa {
+  display: inline-block;
+  font: normal normal normal 14px/1 FontAwesome;
+  font-size: inherit;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}*/
+
+
+#note-input {
+    display:none;
+}
+.note-textarea {
+    max-width:100%;
+}
+.nav-tabs > li > a {
+  font-weight: 600;
+  padding: 10px 25px 10px 25px;
+}
+
+i.fa-globe:hover{
+    color:#1ab394;
+}
+
+/* order status list */
+.point-status {
+    display: inline-block;
+    width: 16.3%;
+    text-align: center;
+}
+
+.status-icon i {
+    font-size: 25px;
+}
+
+.status-icon {
+    margin-bottom: 0px;
+    margin-top: 0;
+    line-height: 55px;
+}
+
+.status-message {
+    font-size: 12px;
+    margin-top: 5px;
+}
+
+.status-line {
+    position: absolute;
+    background: #f1f1f1;
+    height: 3px;
+    width: 100%;
+    top: 35%;
+}
+
+.order-list-status {
+    margin-bottom: 20px;
+    position: relative;
+}
+
+.main-point {
+    width: 50px;
+    height: 50px;
+    margin: 0 auto;
+    border-radius: 50px;
+    z-index: 10;
+    background: #f3f3f4;
+    border: 2px solid #f1f1f1;
+    position: relative;
+}
+
+.active .main-point {
+    background: #1ab394;
+    color: #fff;
+}
+
+@media screen and (max-width: 1075px) {
+    .point-status {
+        width: 16%;
+    }
+}
+
+@media screen and (max-width: 528px) {
+    .order-list-status .fa {
+        /* display: none; */
+        font-size: 18px;
+        top: -10px;
+        position: relative;
+    }
+
+    .point-status {
+        width: 15.5%;
+    }
+
+    .order-list-status .status-message {
+        text-overflow: ellipsis;
+        height: 14px;
+        white-space: nowrap;
+        font-size: 12px;
+        overflow: hidden;
+    }   
+
+    .main-point {
+        width: 35px;
+        height: 35px;
+    }
+
+    .order-status-icon.m-t-md.m-b-md {
+        margin:  0;
+    }
+}
+/* end order status list */
+
+/* CHOK CSS */
+#tab-payment a {
+    border-radius: 0;
+    margin: 0;
+    padding: 10px 30px;
+    margin-left: -1px;
+    border: 0;
+    color: #555;
+}
+
+ul#tab-payment li {
+    border: 1px solid #bbb;
+    margin-left: -1px;
+    background: #ccc;
+    border-bottom: 0;
+}
+
+ul#tab-payment {
+    margin-left: 1px;
+    border-left: 1px solid #e7eaec;
+}
+
+ul#tab-payment>li:first-child {
+    border-left: 0;
+    border-bottom: 0;
+}
+
+#tab-payment li.active a,#tab-payment li a:hover {
+    border-top: 2px solid;
+    background: #fff;
+}
+
+#tab-payment li a {
+    border-top: 2px solid transparent;
+    margin-top: -1px;
+    transition: 100ms;
+}
+
+ul#tab-payment>li:first-child a {
+    border-left: 1px solid #e7eaec;
+}
+
+.ibox-tools a {
+    color:#333;
+}
+
+.action-form-panel.footer-action {
+    padding-left:  0;
+}
+
+ul#tab-payment li {
+ border-right: 1px solid #e7eaec;
+}
+
+.ibox-tools a {
+    margin-left: 0;
+}
+
+.btn-default:active, .btn-default.active, .open .dropdown-toggle.btn-default {
+    box-shadow: none;
+}
+
+@media screen and (max-width: 1024px) {
+    .order-detail-col .col-lg-8 {
+        padding-right: 5px;
+    }
+
+    .order-detail-col h5.m-l-xs.m-r-xs {
+        margin-left: 0;
+    }
+
+    .order-detail-col .col-xs-12.col-lg-12 {
+      padding: 0;
+    }
+
+    .order-detail-col .col-lg-4.text-left {
+        width:  100px;
+        float:  left;
+        font-size: 13px;
+    }
+
+    .title-action {
+        text-align: right;
+        padding-top: 10px;
+    }
+    
+    label.col-xs-2.col-lg-1.text-center.m-b-none {
+        max-width: 20px;
+        font-size: 13px;
+        padding: 0;
+        text-align: left;
+    }
+
+    .table-custom .empty {
+        font-size:  15px;
+    }
+
+    .order-status-icon+.row .col-xs-4 .panel {
+        min-height: auto !important;
+    }
+
+    .order-status-icon+.row .col-xs-4 {
+      width:  100%;
+        margin-bottom:  15px;
+    }
+
+    #print-form button.btn.btn-danger {
+        font-size: 12px;
+    }
+
+    #print-form  button.btn.btn-success {
+        font-size: 12px;
+    }
+
+    #print-form  a.btn.btn-white {
+        font-size:  12px;
+        margin-left: 0;
+    }
+}
+
+@media screen and (max-width: 991px) {
+    .page-heading .col-xs-7 {
+            width:  100%;
+        }
+
+        .page-heading .col-xs-5 {
+            width:  100%;
+            float: left;
+        }
+
+        form#print-form {
+            text-align:  left;
+        }
+}
+
+@media screen and (max-width: 767px) {
+.tabs-container .panel-body {
+    padding: 10px;
+}
+
+.ibox-title {
+    padding: 15px 10px 5px;
+}
+
+.ibox-content {
+    padding: 10px 10px 0px 10px;
+}
+
+#tab-payment a {
+    padding: 10px 10px;
+}
+
+table.table.table-bordered.invoice-tb {
+    min-width:  auto;
+}
+
+.shipment-list {
+    overflow-x: scroll;
+}
+
+div#pay-detail {
+   overflow-x:  scroll;
+}
+
+.ibox-content {
+    overflow-x:  scroll;
+}
+
+table.table {
+    min-width: 675px;
+}
+
+.order-status-icon+.row .col-xs-4 .panel {
+  min-height:  auto !important;
+}
+}
+
+@media screen and (max-width: 480px) {
+.order-status-icon+.row .col-xs-4 {
+    padding-left: 15px;
+    padding-right: 15px;
+}
+
+.order-status-icon.m-t-md.m-b-md {
+    margin:  0;
+}
+
+.ibox-tools {
+    float: left;
+    text-align: left;
+    display: block;
+    width: 100%;
+}
+
+.ibox-title {
+  float: left;
+  width: 100%;
+}
+
+.page-heading h2 {
+    font-size: 16px;
+}
+
+.title-action {
+  padding-top: 5px;
+}
+
+.ibox-tools i, .ibox-tools a i {
+    font-size: 15px !important;
+}
+}
+
+@media screen and (max-width: 360px) {
+
+.order-detail-col .col-lg-4.text-left {
+    padding-right: 25px;
+}
+
+.navbar-top-links li {
+    display: inline-block;
+    /* padding-top: 15px; */
+}
+
+#print-form .btn-group {
+width:  100%;
+margin-bottom: 5px;
+}
+
+#print-form button.btn.btn-danger {
+    width: 100%;
+}
+
+#print-form  button.btn.btn-success {
+    width: 100%;
+}
+
+#print-form  a.btn.btn-white {
+    width: 100%;
+}
+
+.body-small .navbar-top-links li:last-child {
+    margin-right: 0;
+    float: right;
+    padding-top: 18px;
+}
+}
+
+@media screen and (max-width: 528px) {
+  .order-status-icon.m-t-md.m-b-md {
+   margin:  0;
+  }
+}
+
+#print-form .btn-group {
+    margin-bottom:  5px;
+}
+
+.dropdown-menu > li > a.a-disabled{
+    color:#eee;
+}
+.dropdown-menu > li > a.a-disabled:hover{
+    color:#eee;
+}
   </style>    
 
   <?php
@@ -147,6 +533,73 @@ endif;
                                 </div>
                             </div>
                         </div> -->
+
+
+            <div class="order-status-icon m-t-md m-b-md">
+                    <div class="row">
+                                                    <div class="col-sm-12">
+                                <div class="order-list-status">
+                                    <div class="status-line"></div>
+                                    <div class="status-list">
+                                        <div class="point-status active">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-pause-circle-o" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                ยังไม่จ่าย                                          </div>
+                                        </div>
+                                        <div class="point-status active">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-check-circle-o"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                จ่ายแล้ว                                            </div>
+                                        </div>
+                                        <div class="point-status active">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-money"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                ยืนยัน                                          </div>
+                                        </div>
+                                        <div class="point-status active">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-dropbox"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                กำลังแพ็ค                                           </div>
+                                        </div>
+                                        <div class="point-status ">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-truck"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                กำลังจัดส่ง                                         </div>
+                                        </div>
+                                        <div class="point-status ">
+                                            <div class="main-point">
+                                                <div class="status-icon">
+                                                    <i class="fa fa-trophy"></i>
+                                                </div>
+                                            </div>
+                                            <div class="status-message">
+                                                สำเร็จ                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                            </div>
+                </div>
 
 
            <div class="card shadow mb-4">
@@ -298,6 +751,98 @@ endif;
           </div>
 
         </div>
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">รายละเอียดคำสั่งซื้อ</h1>
+
+            <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary"></h6>
+            </div>
+            <div class="card-body">
+          
+          <div class="col-lg-12">
+        <div class="ibox">
+            <div class="ibox-title">
+                <h5>รายการสั่งซื้อ</h5>
+            </div>
+            <div class="ibox-content order-detail-table">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th width="10">#</th>
+                            <th width="60"></th>
+                            <th>ชื่อสินค้า</th>
+                            <th>รหัสสินค้า</th>
+                            <th class="text-right">ราคาขาย</th>
+                            <th class="text-right">จำนวน</th>
+                            <th class="text-right">ส่วนลด</th>
+                            <th class="text-right">จำนวน</th>
+                            <!-- <th class="action text-center" width="70"></th> -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                                                                                                    <tr>
+                                <td>1</td>
+                                <td>
+                                    <div class="order-thumbnail">
+                                        <img src="" alt="">                                  </div>
+                                </td>
+                                <td><a href="/products/5206436" data-pjax="0">ชุดนอน</a>                                </td>
+                                <td>
+                                    <a href="/products/5206436" target="_blank" data-pjax="0" data-toggle="tooltip" data-placement="top" data-original-title="Sample-Product-1608393517"><div>Sample-Product-1608393517</div></a>                               </td>
+                                <!-- <td><a href="/products/5206436" data-pjax="0">Sample-Product-1608393517</a></td> -->
+                                <td class="text-right">299.00</td>
+                                <td class="text-right">2</td>
+                                <td class="text-right">0.00</td>
+                                <td class="text-right">598.00</td>
+                                                                <!-- <td class="action text-center" width="70"> -->
+                                                                <!-- </td> -->
+                            </tr>
+                                            </tbody>
+                                        <tfoot>
+                        <tr>
+                            <td colspan="5" align="right"><strong>รวมทั้งสิ้น / Sub Total</strong></td>
+                            <td colspan="3" align="right">598.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="5" align="right"><strong>ส่วนลด / Discount</strong></td>
+                            <td colspan="3" align="right">0.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="5" align="right"><strong>ขนส่ง / Shipping</strong></td>
+                            <td colspan="3" align="right">50.00</td>
+                        </tr>
+                                                <tr>
+                            <td colspan="5" align="right"><strong>รวมเงินสุทธิ / Net Amount</strong></td>
+                            <td colspan="3" align="right">648.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="5" align="right"><strong>ภาษีมูลค่าเพิ่ม / Vat 7%</strong></td>
+                            <td colspan="3" align="right">0.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="5" align="right"><strong>จำนวนเงินรวม / Total</strong></td>
+                            <td colspan="3" align="right">648.00</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+        </div>
+        </div>
+        </div>
+
+
+
+        
         <!-- /.container-fluid -->
 
 
